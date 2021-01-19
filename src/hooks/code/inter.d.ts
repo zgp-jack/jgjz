18356c701d19c1133f351a0a219023c48ac85bd0
阿鑫 <33578191@qq.com>
2021-01-19 20:36:42 +0800

    合并王帅通讯录 验证码hooks接口修改

--- src/hooks/code/inter.d.ts
+++ src/hooks/code/inter.d.ts
@@ -2,14 +2,14 @@
  * @Author: jsxin
  * @Date: 2021-01-18 16:30:03
  * @LastEditors: jsxin
- * @LastEditTime: 2021-01-18 18:03:40
+ * @LastEditTime: 2021-01-19 20:35:27
  * @Description: 获取验证码的返回数据类型
  */
 
 import { Result } from '@/utils/request/inter.d'
 
 // 获取验证码  接口返回的数据类型
-export default interface UserGetCodeResult extends Result<null>{
+export default interface UserGetCodeResult extends Result<number>{
   /** 多少秒后可以重新获取验证码 */
   refresh: number
 }

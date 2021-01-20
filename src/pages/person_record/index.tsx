066efa62b54d3b659460e5e3c6a7d00a4784ed05
zhang_guoping <jackzgp@aliyun.com>
2021-01-20 14:02:51 +0800

    记工记账 个人记工页面

--- src/pages/person_record/index.tsx
+++ src/pages/person_record/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { Config, useState } from '@tarojs/taro'
 import { View, Text, Image, Textarea, Button, Input } from '@tarojs/components'
 import { observer, useLocalStore } from '@tarojs/mobx'
+import MoreWorkTime from '../person_tally/components/moreworktime/index'
 import './index.scss'
 
 let type = [
@@ -8,30 +9,64 @@ let type = [
     {name:'记工量',typenum:1},
     {name:'记工钱',typenum:2},
 ];
+let worknum = [
+    {name:'一个工', selected: true, id:"onework"},
+    {name:'半个工', selected: false, id:"halfwork"},
+    {name:'休息', selected: false, id:"rest"},
+    {name:'0小时', selected: false, id:"morework"}
+];
+let overnum = [
+    {name:'无加班', selected: true, id:"novertime"},
+    {name:'0小时', selected: false, id:'moreovertime'}
+];
 let record_type:string = "记工天";
 function PersonRecord(){
-    const config = {
-        navigationBarTitleText: '个人' + record_type
-    }
+    // 切换记工type
     const [recordnum, setRecordnum] = useState<number>(type[0].typenum);
+    // input 交互值
     const [quantity, setQuantity] = useState<string>('0');
+    // 选中input 样式
     const [amountInput, setAmountIput] = useState<boolean>(false);
+    // 上班时间默认数据
+    const [worktime, setWorkTime] = useState(worknum);
+    // 加班时间默认数据
+    const [overtime, setOverTime] = useState(overnum);
+    // 是否显示 加班时间
+    const [isovertime, setIsovetTime] = useState<boolean>(true);
+    //是否显示加班组件
+    const [isMoreWorkTime, setIsMoreWorkTime] = useState<boolean>(false);
 
     const Changetype = (e) => {
         switch(e.target.id){
             case "recordDay":
                 setRecordnum(type[0].typenum);
-                config.navigationBarTitleText = '个人' + type[0].name;
                 break;
             case "recordamout":
                 setRecordnum(type[1].typenum);
-                config.navigationBarTitleText = '个人' + type[1].name;
                 break;
             case "recordmoney":
                 setRecordnum(type[2].typenum);
-                config.navigationBarTitleText = '个人' + type[2].name;
                 break;
         }
+    }
+    const SelectWorkTime = (e) => {
+        if(!e.target.id) return false;
+        let worklist = JSON.parse(JSON.stringify(worktime));
+        worklist.forEach((item) => e.target.id == item.id ? item.selected = true:item.selected = false)
+        setWorkTime(worklist)
+    }
+    const SelectMoreWork = (e) => {
+        e.stopPropagation()
+        setIsMoreWorkTime(true);
+    }
+    const SelectOverTime = (e) => {
+        if(!e.target.id) return false;
+        let overlist = JSON.parse(JSON.stringify(overnum));
+        overlist.forEach((item) => e.target.id == item.id ? item.selected = true:item.selected = false)
+        setOverTime(overlist);
+    }
+    const WorktimeCancle = (e) => {
+        setIsMoreWorkTime(false);
     }
     return (
         <View className="person-record">
@@ -42,23 +77,23 @@ function PersonRecord(){
             </View>
             {recordnum == 0 && <View>
             <View className="person-record-time">
-                <View className="person-record-worktime">
+                <View className="person-record-worktime" onClick={SelectWorkTime}>
                     <Text className="worktime-text">上班时长</Text>
-                    <View className="worktime worktime-active">1个工</View>
-                    <View className="worktime">半个工</View>
-                    <View className="worktime">休息</View>
-                    <View className="worktime worktime-select">
-                        <Text className="worktime-select-time">0小时</Text>
+                    <View className={worktime[0].selected?"worktime worktime-active":"worktime"} id="onework">1个工</View>
+                    <View className={worktime[1].selected?"worktime worktime-active":"worktime"} id="halfwork">半个工</View>
+                    <View className={worktime[2].selected?"worktime worktime-active":"worktime"} id="rest">休息</View>
+                    <View className={worktime[3].selected?"worktime worktime-active worktime-select":"worktime worktime-select"} onClick={SelectMoreWork}>
+                        <Text className="worktime-select-time" id="morework">0小时</Text>
                     </View>
                 </View>
-                <View className="person-record-overtime person-record-worktime">
+                {isovertime && <View className="person-record-overtime person-record-worktime" onClick={SelectOverTime}>
                     <Text className="worktime-text">加班时长</Text>
-                    <View className="worktime">无加班</View>
-                    <View className="worktime worktime-select worktime-active">
-                        <Text className="worktime-select-time">24小时</Text>
+                    <View className={overtime[0].selected?"worktime worktime-active":"worktime"} id="novertime">无加班</View>
+                    <View className={overtime[1].selected?"worktime worktime-select worktime-active":"worktime worktime-select"}>
+                        <Text className="worktime-select-time" id="moreovertime">24小时</Text>
                     </View>
-                    <Text className="overtime-icon"></Text>
-                </View>
+                    <Text className="overtime-icon" onClick={(e) => {e.stopPropagation();setIsovetTime(false)}}></Text>
+                </View>}
             </View>
             <View className="person-record-overtime person-record-date">
                 <Image className="person-record-date-img" src="https://jgjz.oss-cn-beijing.aliyuncs.com/new_mini/images/gl/Bookkeeping-icon.png" />
@@ -80,7 +115,7 @@ function PersonRecord(){
             <View className="person-record-component">
                 <View className="person-record-component-item">今天11月2日</View>
                 <View className="person-record-component-item">班组长</View>
-                <View className="person-record-component-item">加班时长</View>
+                {!isovertime && <View className="person-record-component-item">加班时长</View>}
             </View> </View>}
             {recordnum == 1 && <View className="person-record-work">
                 <View className={amountInput?"work-amount input-active":"work-amount"}>
@@ -151,6 +186,7 @@ function PersonRecord(){
                 <Button className="person-record-resave">保存并再记一笔</Button>
                 <Button className="person-record-save">确认记工</Button>
             </View>
+            {isMoreWorkTime && <MoreWorkTime WorktimeCancle={WorktimeCancle} />}
         </View>
     )
 }

66c7d721513dcc13e22441d12ec5c9e8143d1e13
阿鑫 <33578191@qq.com>
2021-01-21 20:20:42 +0800

    新增常用 函数

--- src/utils/index/index.ts
+++ src/utils/index/index.ts
@@ -2,9 +2,11 @@
  * @Author: jsxin
  * @Date: 2021-01-21 13:44:17
  * @LastEditors: jsxin
- * @LastEditTime: 2021-01-21 13:46:55
+ * @LastEditTime: 2021-01-21 20:19:43
  * @Description: 常用助手函数
  */
+
+import { showModal } from '@/utils/msg';
 
 /**
  * @name: objDeepCopy for jsxin
@@ -18,4 +20,31 @@ export function objDeepCopy(source: any): any {
     sourceCopy[item] = typeof source[item] === 'object' ? objDeepCopy(source[item]) : source[item];
   }
   return sourceCopy;
+}
+
+/**
+ * @name: copyWechat for jsxin
+ * @params wechat: string 需要复制的微信号码
+ * @return void 无返回值
+ * @description 复制微信号并提示
+*/
+export function copyWechat(wechat: string): void {
+  Taro.setClipboardData({
+    data: wechat,
+    success: () => {
+      showModal(`微信号${wechat}已经成功复制到您的粘贴板，快去打开微信-添加朋友吧~!`)
+    }
+  })
+}
+
+/**
+ * @name: callPhone for jsxin
+ * @params phone: string 需要拨号的号码
+ * @return void 无返回值
+ * @description 拨打电话
+*/
+export function callPhone(phone: string): void {
+  Taro.makePhoneCall({
+    phoneNumber: phone
+  })
 }

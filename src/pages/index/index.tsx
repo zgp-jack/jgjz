f5f6e0a6d79b9f53685f75f169b8bfeccda51a91
阿鑫 <33578191@qq.com>
2021-01-22 20:33:39 +0800

    events 事件管理器

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,14 +1,21 @@
-import Taro from '@tarojs/taro'
+import Taro, { useEffect, eventCenter } from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
 
 export default function Index(){
 
+  useEffect(() => {
+    eventCenter.on('test',(aa: string) => {
+      console.log('我被触发了...',aa)
+    })
+    return () => eventCenter.off('test')
+  },[])
+
   return (
     <View className='index'>
       <Text onClick={() => console.log('您好，程序员！')}>Hello world!</Text>
       <Button onClick={ () => Taro.navigateTo({
-        url: '/pages/test/index'
+        url: '/pages/example_mobx/index'
       })}> go to mobx page!</Button>
       <Button onClick={ () => Taro.navigateTo({
         url: '/pages/person_record/index'

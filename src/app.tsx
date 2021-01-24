6b67ddadc9e0f5d2e65338c809e3ab0fb6696afe
阿鑫 <33578191@qq.com>
2021-01-24 14:56:49 +0800

    新增 流水详情（借支、支出）查看、删除、修改

--- src/app.tsx
+++ src/app.tsx
@@ -51,6 +51,9 @@ class App extends Component {
    */
   config: Config = {
     pages: [
+      'pages/personl_business/expenditure/index',
+      'pages/personl_business/borrow/index',
+      
       'pages/remember/index',
       'pages/login/index', //登录
       'pages/work_team/record_work/index',
--- src/pages/person_detail/index.tsx
+++ src/pages/person_detail/index.tsx
@@ -1,4 +1,4 @@
-import Taro from '@tarojs/taro'
+import Taro, { useRouter } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import ModifyBorrow from './modify_borrow/index'
 import ModifyExpenditure from './modify_expenditure/index'
@@ -7,21 +7,24 @@ import ModifyDay from './modify_day/index'
 import ModifyAmoumt from './modify_amount/index'
 import './index.scss'
 
-function PersonDetail(){
-    /* type(0)--支出  type(1)--借支  type(2)--工天   type(3)--工钱  type(4)--工量
-    */
-    const type:number = 0;
+export default function PersonDetail(){
+
+  const router = useRouter()
+  /** 类型 type(1)--支出  type(2)--借支  type(3)--工天   type(4)--工钱  type(5)--工量 */
+  const { type = '2',id = '' } = router.params
+  console.log(type, id)
+
     return (
         <View>
-          {type == 0 && <ModifyBorrow />}
-          {type == 1 && <ModifyExpenditure />}
-          {type == 2 && <ModifyDay />}
-          {type == 3 && <ModifyMoney />}
-          {type == 4 && <ModifyAmoumt />}
+          {type == '1' && <ModifyBorrow />}
+          {type == '2' && <ModifyExpenditure />}
+          {type == '3' && <ModifyDay />}
+          {type == '4' && <ModifyMoney />}
+          {type == '5' && <ModifyAmoumt />}
         </View>
     )
 }
-export default PersonDetail
+
 PersonDetail.config = {
     navigationBarTitleText: '修改借支',
 }

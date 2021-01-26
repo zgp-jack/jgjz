870eb3d2fbd57461554e90c375f3e5b068056d4a
阿鑫 <33578191@qq.com>
2021-01-26 14:41:22 +0800

    用户信息初始化

--- src/store/user/index.ts
+++ src/store/user/index.ts
@@ -2,17 +2,21 @@
  * @Author: jsxin
  * @Date: 2021-01-18 15:29:16
  * @LastEditors: jsxin
- * @LastEditTime: 2021-01-18 15:29:17
+ * @LastEditTime: 2021-01-26 14:06:21
  * @Description: 用户信息管理器
  */
- 
+import Taro from '@tarojs/taro'
 import { action, observable } from 'mobx'
 import UserInfo from './inter.d'
+import { UserInfo as user } from '@/config/store'
+
+// 初始化用户信息
+let userInfo: UserInfo = Taro.getStorageSync(user)
 
 export class User {
   /** 用户信息初始化 */
   @observable
-  user: UserInfo = {
+  user: UserInfo = userInfo ? userInfo : {
     token: '',
     userId: 0,
     login: false,

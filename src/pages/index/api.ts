785c32347384fe50500409cb732d4b487a0c7263
yangchao <1206662047@qq.com>
2021-01-27 11:09:50 +0800

    冲突解决

--- src/pages/index/api.ts
+++ src/pages/index/api.ts
@@ -1,10 +1,6 @@
 import {get} from "@/utils/request";
 import {getCountUrl, useGetIndexBusiness, userGetBusinessLists} from "@/utils/api";
-<<<<<<< Updated upstream:src/pages/index/api.ts
 import {GetCountParams, GetCountResult, getRememberParams, getRememberResult} from "@/pages/index/inter";
-=======
-import {GetCountParams, GetCountResult, getRememberParams, getRememberResult} from "@/pages/remember/inter";
->>>>>>> Stashed changes:src/pages/remember/api.ts
 import {GetWorkFlowResult} from '@/pages/work_team/team_record/index.d'
 
 export function getBusiness(params: GetCountParams) {

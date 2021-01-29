257c5daf966251527f0a66f625b114f225df8d59
uYoaiXiL <Rainy_fsar@163.com>
2021-01-29 16:35:36 +0800

    修改杨朝bug

--- src/components/flow/work_count_day/index.tsx
+++ src/components/flow/work_count_day/index.tsx
@@ -27,20 +27,24 @@ export default function WorkCountDay({list = [], type = 1}: PropsData) {
         <Block key={item.id}>
           <View className='bokkeeping-list-container'>
             <View className='bokkeeping-list-item' onClick={() => goDetail(item.id, item.business_type)}>
-              <View className='bokkeeping-list-left'>
-                <View className='bokkeeping-list-title'>{item.worker_name}</View>
-                <View className='bokkeeping-list-des'>{type == 1 ? '工天' : (type == 2 ? '工量' : '')}</View>
-                {item.is_note && <View className='bokkeeping-list-remarks'>备注：{item.note}</View>}
-              </View>
-              <View className='bokkeeping-list-right'>
-                <View className='bokkeeping-list-count'>
-                  {type == 2 ? <View>{item.unit_num + item.unit}</View> :
-                    <View>上班：{item.work_time ? item.work_time + '个工' : ''}{item.work_time_hour ? item.work_time_hour + '小时' : ''}{(!item.work_time && !item.work_time_hour) ? '休息' : ''}</View>}
-                  {item.overtime && <View>加班：{item.overtime}小时</View>}
+              <View className="bokkeeping-list-item-box">
+                <View className='bokkeeping-list-left'>
+                  <View className='bokkeeping-list-title'>{item.worker_name}</View>
+                  <View className='bokkeeping-list-des'>{type == 1 ? '工天' : (type == 2 ? '工量' : '')}</View>
                 </View>
-                <Image src={`${IMGCDNURL}common/arrow-right.png`}></Image>
+                <View className='bokkeeping-list-right'>
+                  <View className='bokkeeping-list-count'>
+                    {type == 2 ? <View>{item.unit_num + item.unit}</View> :
+                      <View>上班：{item.work_time ? item.work_time + '个工' : ''}{item.work_time_hour ? item.work_time_hour + '小时' : ''}{(!item.work_time && !item.work_time_hour) ? '休息' : ''}</View>}
+                    {item.overtime && <View>加班：{item.overtime}小时</View>}
+                  </View>
+                  <Image src={`${IMGCDNURL}common/arrow-right.png`}></Image>
+                </View>
               </View>
+              {item.is_note && <View className='bokkeeping-list-remarks'>备注：{item.note}</View>}
+
             </View>
+
           </View>
         </Block>))}
     </Block>

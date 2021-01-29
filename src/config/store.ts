25749b59776e0422ee0088f49f3a45edb959e687
阿鑫 <33578191@qq.com>
2021-01-29 14:06:53 +0800

    区分记工记账 历史数据

--- src/config/store.ts
+++ src/config/store.ts
@@ -2,7 +2,7 @@
  * @Author: jsxin
  * @Date: 2021-01-18 14:00:27
  * @LastEditors: jsxin
- * @LastEditTime: 2021-01-28 10:56:48
+ * @LastEditTime: 2021-01-29 14:00:00
  * @Description: 项目缓存文件，所有缓存key存放文件 
  ! @rules: 1.key 为大驼峰 值为小驼峰 2.注释在上方
  */
@@ -11,13 +11,30 @@
 export const UserInfo: string = 'userInfo'
 // 最后进入的记工本信息
 export const LastJoinAccountBookInfo: string = 'lastJoinAccountBookInfo'
-// 个人历史班组长信息
-export const PersonlHistoryGroupLeader: string = 'personlHistoryGroupLeader'
-// 个人历史分项信息
-export const PersonlHistoryClassitifySubitem: string = 'personlHistoryClassitifySubitem'
-// 个人历史分类信息
-export const PersonHistoryClassitify: string = 'personHistoryClassitify'
-// 历史分类信息
-export const HistoryClassifyType: string = 'historyClassifyType'
-// 历史分项信息
-export const HistoryClassifySubitem: string = 'historyClassifySubitem'
+// 个人记工天-历史班组长信息
+export const PersonlWorkdayHistoryGroupLeader: string = 'personlWorkdayHistoryGroupLeader'
+// 个人记工钱-历史班组长信息
+export const PersonlMoneyHistoryGroupLeader: string = 'personlMoneyHistoryGroupLeader'
+
+// 个人记工量-历史班组长信息
+export const PersonlAmountHistoryGroupLeader: string = 'personlAmountHistoryGroupLeader'
+// 个人记工量-历史分项信息
+export const PersonlAmountHistoryClassitifySubitem: string = 'personlAmountHistoryClassitifySubitem'
+
+// 个人记借支-历史班组长信息
+export const PersonlBorrowHistoryGroupLeader: string = 'personlBorrowHistoryGroupLeader'
+// 个人记借支-历史分类信息
+export const PersonlBorrowHistoryClassifyType: string = 'personlBorrowHistoryClassifyType'
+
+// 个人记支出-历史班组长信息
+export const PersonlExpenditureHistoryGroupLeader: string = 'personlExpenditureHistoryGroupLeader'
+// 个人记支出-历史分类信息
+export const PersonlExpenditureHistoryClassifyType: string = 'personlExpenditureHistoryClassifyType'
+
+// 班组记工量-历史分项信息
+export const GroupAmountHistoryClassitifySubitem: string = 'groupAmountHistoryClassitifySubitem'
+// 班组记借支-历史分类信息
+export const GroupBorrowHistoryClassitifyType: string = 'groupBorrowHistoryClassitifyType'
+// 班组记支出-历史分类信息
+export const GroupExpenditureHistoryClassitifyType: string = 'groupExpenditureHistoryClassitifyType'
+
